a7ec36cbeb9b3faaa3440596125ab7882a3034ce
RonaldZenteno <549dhenisz@gmail.com>
2024-11-13 23:16:57 +0100

    activando boton de cargar mas

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ loadMoreBtn1.onclick = () => {
         loadMoreBtn1.style.display = 'none'
     }
 }
+
+let loadMoreBtn2 = document.querySelector('#load-more-2');
+let currentItem2 = 4;
+
+loadMoreBtn2.onclick = () => {
+    let boxes = [...document.querySelectorAll(
+        '.box-container-2 .box-2'
+    )];
+    for(var i=currentItem2; i <currentItem2+4 ; i++){
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem2 +=4;
+    if(currentItem2 >= boxes.length){
+        loadMoreBtn2.style.display = 'none'
+    }
+}
+
+let loadMoreBtn3 = document.querySelector('#load-more-3');
+let currentItem3 = 4;
+
+loadMoreBtn3.onclick = () => {
+    let boxes = [...document.querySelectorAll(
+        '.box-container-3 .box-3'
+    )];
+    for(var i=currentItem3; i <currentItem3+4 ; i++){
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem3 +=4;
+    if(currentItem3 >= boxes.length){
+        loadMoreBtn3.style.display = 'none'
+    }
+}
